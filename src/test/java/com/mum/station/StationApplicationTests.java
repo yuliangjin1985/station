f371b60467393b7124ee799dfc6d61cd7650cefa
靳玉亮 <leo@air.local>
2018-10-24 18:32:04 -0500

    initialize the project

---
+++ src/test/java/com/mum/station/StationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mum.station;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
