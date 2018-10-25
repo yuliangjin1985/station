6507ac12df2abd4eeeb9e2b7cf01257ad241d365
靳玉亮 <leo@air.local>
2018-10-25 13:54:13 -0500

    add controller test cases

--- src/test/java/com/mum/station/StationControllerTest.java
+++ src/test/java/com/mum/station/StationControllerTest.java
@@ -1,11 +1,14 @@
 package com.mum.station;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +27,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(StationController.class)
@@ -47,5 +54,68 @@ public class StationControllerTest {
         .andExpect(status().isOk())
         .andExpect(jsonPath("name").value("station Name"))
         .andExpect(jsonPath("hdEnabled").value(true));
+  }
+
+  @Test
+  public void testRemoveStation() throws Exception {
+    Station station = new Station(2l, "station Name", "station 01003", true, "call sign");
+    when(stationService.removeStation(any())).thenReturn(true);
+    mockMvc.perform(delete("/station")
+        .param("stationId", "station 01003"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect((ResultMatcher) content().string(containsString("Successful")));
+  }
+
+  @Test
+  public void testUpdateStation() throws Exception {
+    Station station = new Station(2l, "station Name", "station 01003", true, "call sign");
+    String requestStr = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(station);
+    when(stationService.updateStation(any())).thenReturn(true);
+    mockMvc.perform(put("/station")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestStr))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect((ResultMatcher) content().string(containsString("Successful")));
+  }
+
+  @Test
+  public void testSearchByName() throws Exception {
+    Station station = new Station(2l, "station Name", "station 01003", true, "call sign");
+    String requestStr = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(station);
+    when(stationService.getByName(any())).thenReturn(station);
+    mockMvc.perform(get("/station/name")
+        .param("name", "station name"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect((ResultMatcher) content().string(containsString("Successful")));
+  }
+
+  @Test
+  public void testSearchByStationId() throws Exception {
+    Station station = new Station(2l, "station Name", "station 01003", true, "call sign");
+    String requestStr = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(station);
+    when(stationService.getByStationId(any())).thenReturn(station);
+    mockMvc.perform(get("/station/stationId")
+        .param("stationId", "station 01003"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect((ResultMatcher) content().string(containsString("Successful")));
+  }
+
+  @Test
+  public void testSearchHdEnabled() throws Exception {
+    Station station = new Station(2l, "station Name", "station 01003", true, "call sign");
+    Station station1 = new Station(3l, "station Name", "station 01003", true, "call sign");
+    List<Station> list = new ArrayList<>();
+    list.add(station);
+    list.add(station1);
+    String requestStr = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(station);
+    when(stationService.searchedHdEnbaled(any())).thenReturn(list);
+    mockMvc.perform(get("/station/hds"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect((ResultMatcher) content().string(containsString("Successful")));
   }
 }
